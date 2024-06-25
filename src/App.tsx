fd6544b6526fcce7a1978b6b1f00429b6d9dfdaf
João Gomes <jowgomes3@gmail.com>
2024-06-24 21:54:53 -0300

    aprendendo a usar typescript com hooks no react

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useMemo, useCallback } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import TextField from "./components/TextField";
+
+interface Task {
+  id: string;
+  isCompleted: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [count, setCount] = useState<number>(1);
+
+  const completedTasks: boolean[] = useMemo(() => {
+    return tasks
+      .filter((task) => task.isCompleted)
+      .map((task) => task.isCompleted);
+  }, [tasks]);
+
+  console.log(completedTasks);
+
+  const handleButtonClick: () => void = useCallback(() => {
+    console.log("clicked");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button
+          theme="dark"
+          onClick={handleButtonClick}
+          options={{ variant: "outlined" }}
+        />
+        <TextField onChange={(e) => e.target.value} />
       </header>
     </div>
   );
